5671bbb0973adbcf5fbbf0aca1968d3ae557a501
Daniel Vogel <54858106+vogelcodes@users.noreply.github.com>
2024-03-15 20:25:53 +0000

    get-leads

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"io"
+	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -75,6 +79,18 @@ type Page struct {
     Form FormData
 }
 
+type Lead struct {
+    Email string
+    Phone string
+    Name string
+    Tag string
+    Date string
+    Cta string
+    Url string
+    AvatarUrl string
+    Location string
+}
+
 func newPage() Page {
     return Page{
         Data: newData(),
@@ -96,6 +112,47 @@ func main() {
 
     e.GET("/", func(c echo.Context) error {
         return c.Render(200, "index", page)
+    })
+    e.GET("/leads", func(c echo.Context) error {
+        response, err := http.Get("https://script.google.com/macros/s/AKfycbwIAj1HWYmqEeF7I_A3WfJGoshnPzSbQLDYir00RhgoWs1QsRj5nLAsEUIAGYuD7DfopQ/exec")
+        if err != nil {
+            fmt.Println(err)
+            return c.String(500, "Erro ao buscar leads") // HTTP Status 500 para erro 
+        }
+        defer response.Body.Close() // Garante o fechamento do Body
+    
+        bodyBytes, err := io.ReadAll(response.Body)
+        var leads [][]interface{}   
+        json.Unmarshal(bodyBytes, &leads)
+        //remove the first element
+        leads = leads[1:]
+        //create a new array with struct Lead iterating over the leads
+        var leadsStruct []Lead
+        for _, lead := range leads {
+            var cta string
+            switch v := lead[5].(type) {
+            case float64:
+                cta = strconv.FormatFloat(v, 'f', -1, 64)
+            case string:
+                cta = v
+            default:
+                // handle error
+            }
+            
+            leadsStruct = append(leadsStruct, Lead{
+                Email: lead[0].(string),
+                Phone: lead[1].(string),
+                Name: lead[2].(string),
+                Tag: lead[3].(string),
+                Date: lead[4].(string),
+                Cta: cta,
+                Url: lead[6].(string),
+                AvatarUrl: lead[7].(string),
+                Location: lead[8].(string),
+            })
+        }
+        
+        return c.JSON(200, leadsStruct[0])
     })
 
     e.POST("/contacts", func(c echo.Context) error {
